29ef15397f13b114f376c64a5e16af0d4269733e
manfredjoa <joamanfred@gmail.com>
2025-06-18 16:17:48 -0400

    remove black and white from Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,18 @@
 export const UniversalColors = {
-  // Grays
   lightGray: "#f5f5f5",
   medLightGray: "#eee",
   medGray: "#999",
   darkMedGray: "#666",
   darkGray: "#333",
 
-  // Yellows
   lightYellow: "#fef3c7",
   darkYellow: "#f59e0b",
 
-  // Greens
   lightGreen: "#f0fdf4",
   darkGreen: "#16a34a",
 
-  // Reds
   metRed: "#b60021",
   favoriteRed: "#ff4444",
-
-  // Basic colors
-  white: "#ffffff",
-  black: "#000000",
 } as const;
 
 export const Colors = {
