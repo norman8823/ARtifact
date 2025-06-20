66a6e72666d912a52360bf2c005f18f84d34a20a
manfredjoa <joamanfred@gmail.com>
2025-06-20 12:11:58 -0400

    update styling on profile settings screen

--- app/(dashboard)/profile.tsx
+++ app/(dashboard)/profile.tsx
@@ -602,7 +602,7 @@ const styles = StyleSheet.create({
   },
   modalOverlay: {
     flex: 1,
-    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
     justifyContent: "center",
     alignItems: "center",
   },
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout() {
           <Stack.Screen
             name="profileSettings"
             options={{
-              headerTitle: "",
+              headerTitle: "Profile Settings",
             }}
           />
           <Stack.Screen
