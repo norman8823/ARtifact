a25011a0610bd46d0ccf38687cf658ba2007b143
Raul Jiminian <raul.e.jiminian@gmail.com>
2025-08-12 10:37:38 -0400

    import necessary packages to app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,6 @@
+import "react-native-gesture-handler";
+import "react-native-get-random-values";
+
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
